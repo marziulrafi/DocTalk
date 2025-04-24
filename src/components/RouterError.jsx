b9d774237ffcc12627d03e3cabb2a66d5e22d01e
K M Marziul Karim <marziulrafi@gmail.com>
2025-04-24 21:25:02 +0600

    Error Message updated

--- src/components/RouterError.jsx
+++ src/components/RouterError.jsx
@@ -6,7 +6,8 @@ const RouterError = () => {
 
   const navigate = useNavigate();
   return (
-    <div className="w-96 mx-auto">
+    <div className="w-96 mx-auto mt-8">
+      <h1 className="text-2xl mb-6 text-center font-semibold">No Doctor Found!!</h1>
       <img src={Err} alt="" />
       <button onClick={() => navigate("/")} className="btn bg-[#176AE5] text-white rounded-full flex mx-auto mt-4 mb-4">View All Doctors</button>
     </div>
