71d0a492b3666728d82e471b2ff8dcae85f2e90a
K M Marziul Karim <marziulrafi@gmail.com>
2025-04-24 20:05:37 +0600

    blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -5,7 +5,7 @@ const Blog = ({blog}) => {
     const {question, answer} = blog;
 
     return (
-        <div className="border-2 border-[#ffffff80] rounded-3xl px-5 py-4 bg-linear-to-b from-[#EFEFEF] to-white mb-4 w-3/4 mx-auto">
+        <div className="border-2 border-[#ffffff80] rounded-3xl px-5 py-4 bg-white mb-4 w-3/4 mx-auto">
             <h2 className="text-2xl font-bold">Q. {question}</h2>
             <p><span className="font-bold text-xl">Ans: </span>{answer}</p>
         </div>
